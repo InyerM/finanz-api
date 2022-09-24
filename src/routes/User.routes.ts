cf91cdf84b1797249b05a05f43f7f7216f012947
Juan Daniel Valencia <112526775+nielvadev@users.noreply.github.com>
2022-09-23 22:04:38 -0500

    Create delete and update user api

--- src/routes/User.routes.ts
+++ src/routes/User.routes.ts
@@ -2,10 +2,13 @@ import express from 'express'
 
 const router = express.Router()
 
-import { getUsers, createUser, authUser } from '../controllers/UserController'
+import { getUsers, createUser, authUser, getUser, editUser, deleteUser } from '../controllers/UserController'
 
 router.get('/user', getUsers)
 router.post('/user', createUser)
 router.post('/user/auth', authUser)
+router.get('/user/:id', getUser)
+router.put('/user/:id', editUser)
+router.delete('/user/:id', deleteUser)
 
 export default router
